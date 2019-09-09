156dcb5655a2320a8bfde2e6b92e29e354a61ee5
Your Name <you@example.com>
2019-09-09 11:55:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+function changeBold(){
+
+var xx = document.getElementById("xx-id");
+
+
+if (xx.style.fontWeight==="normal")
+{xx.style.fontWeight="bold"}
+else {xx.style.fontWeight="normal"}
+}
+
+
+
